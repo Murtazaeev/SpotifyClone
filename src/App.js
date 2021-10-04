08f3ee088f4171b2dd4f85c7024dedff9978689b
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2021-10-05 00:08:15 +0200

    starting body

--- src/App.js
+++ src/App.js
@@ -4,37 +4,38 @@ import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
 import Player from './Player'
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-
-  const [token, setToken] = useState(null);
-
-
-
+  const [{ user, token }, dispatch] = useDataLayerValue();
   
-  //running the code on a given condition
   useEffect(() => {
         const hash = getTokenFromUrl();
         window.location.hash = "";
         const _token = hash.access_token;
 
         if(_token) {
-          setToken(_token);
-
+          dispatch({
+            type: "SET_TOKEN",
+            token: _token,
+          });
           spotify.setAccessToken(_token);
 
           spotify.getMe().then(user => {
-            console.log('person', user);
+           
+            dispatch({
+              type: 'SET_USER',
+              user: user,
+            })
           });
         }
-        console.log(' there is a token', token)
   }, []);
 
   return (
     <div className="app">
-      {token ? <Player /> : <Login /> 
+      {token ? <Player spotify={spotify} /> : <Login /> 
       }
     </div>
   );
--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,8 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
+    //remove after finished developing!!!!!!!!!!!!!
+    token: "BQAJs0T0wnDq84QZIC2n5RzJh3D9L0PQSpOwtCPpwVt6ugWKCUf6UqmQhnLwWr-NmVo9hUB2y3U8O54-5SdoJAWzxJlyCjuB1wvp8PIWzHaR1SRHaWBBf0R-28Tb3RXMH32WdEYGhiu677bKQKEvOcg6f18Z97hx25OUI6xfOcaMsQJVnWHF"
 }
 
 const reducer = (state, action) => {
@@ -13,6 +15,11 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 user: action.user
+            };
+            case 'SET_TOKEN': 
+            return{
+                ...state,
+                token: action.token,
             }
             default: 
             return state;
